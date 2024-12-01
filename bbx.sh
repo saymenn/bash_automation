5feff5f148474716bbc1c5c097bb6b550ea34ce6
saymenn <ben.aymen.1337@googlemail.com>
2024-12-01 21:30:11 +0100

    added crawling, screenshoting etc features

--- bbx.sh
+++ bbx.sh
@@ -10,16 +10,32 @@ resolved_path=$name".all.resolved"
 bruteforced_path=$name".all.bruteforced"
 final_list=$name".all.final_list"
 webservers_path=$name".all.webservers"
+webscreenshots_path=$name".all.screenshots"
+katana_path=$name".all.katana"
+gau_path=$name".all.gau"
+js_subdomains_path=$name".all.js_subdomains"
 
 subfinder -dL $roots -all -o $subfinder_path;
-cat $subfinder_path | puredns resolve --write $resolved_path
+cat $subfinder_path | puredns resolve --write $resolved_path;
 
-if [ $bruteforce = brute]; then
-    puredns bruteforce /opt/SecLists/Discovery/DNS/subdomains-top1million-110000.txt -d $resolved_path --write $bruteforced_path
+if [ $bruteforce = brute ]; then
+    puredns bruteforce /opt/SecLists/Discovery/DNS/subdomains-top1million-110000.txt -d $resolved_path --write $bruteforced_path;
     cat $resolved_path $bruteforced_path | sort -u | tee $final_list
 else
-    cat $resolved_path | sort -u | tee $final_list
+    cat $resolved_path | sort -u | tee $final_list;
 fi
 
 # webservers
-cat $final_list | httprobe -p http:80 -p https:443 -p http:8080 -p https:9443 -p https:8443 -p http:8081 -p http:8000 -p http:3000 -p http:10000 | tee $webservers_path
+cat $final_list | httprobe -p http:80 -p https:443 -p http:8080 -p https:9443 -p https:8443 -p http:8081 -p http:8000 -p http:3000 -p http:10000 | tee $webservers_path;
+
+# screenshots
+cat $webservers_path | httpx -st 30 -timeout 30 -ss -o $webscreenshots_path;
+
+# crawling
+katana -list $webservers_path -duc -silent -nc -jc -jsl -kf -fx -xhr -ef woff,css,png,svg,jpg,woff2,jpeg,gif,svg -o $katana_path;
+
+# sub list of subdomains found in js files
+cat $katana_path | grep -oP 'https?://[a-zA-Z0-9.-]+(:[0-9]+)?/' | sort -u | tee $js_subdomains_path;
+
+# archive links
+cat $webservers_path | gau --o $gau_path;
